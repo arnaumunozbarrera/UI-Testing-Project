1e40e9ca7f5f8daa11adaffd74d8d9731edf1858
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2024-12-06 17:25:29 +0100

    Update test and develoment code to UserRegister

--- src/test/java/features/userregister.feature
+++ src/test/java/features/userregister.feature
@@ -5,8 +5,8 @@ Given the user is in the index page
 When the user clicks on user menu 
 Then the lateral menu tab info appears
 
-Scenario: Show Register form
-Given the user clicks on user menu
+Scenario: Show Register Form
+Given the lateral menu tab info appears
 When the user clicks on register
 Then the user register form appears
 
@@ -14,10 +14,10 @@ Scenario: Show Register Error
 Given the user register form appears
 When the user completes register form incorrectly
 And the user clicks on submit
-Then the user register message appears
+Then the user fatal register message appears
 
 Scenario: Show Register Success
 Given the user register form appears
 When the user completes register form correctly
 And the user clicks on submit
-Then the user register message appears
+Then the user verification register message appears
--- src/test/java/steps/UserRegisterSteps.java
+++ src/test/java/steps/UserRegisterSteps.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 
@@ -18,26 +19,54 @@ public class UserRegisterSteps {
 	
 	@When("the user clicks on register")
 	public void theUserClicksOnRegister() {
-
+		driver.findElement(By.id("myaccount-dropdown-register-button")).click();		
 	}
 	
 	@Then("the user register form appears")
 	public void theUserRegisterFormAppears(){
-		// Test ToDo
+		 WebElement form = driver.findElement(By.xpath(
+			        "//form[@aria-label='Formulario de inscripción Paso 1 - Correo electrónico' " +
+			        "and @data-test='mms-myacc-registration-email-form' and @method='post' and @novalidate='']"
+			    ));
+	    assertTrue(form != null);
 	}
 	
 	@When("the user completes register form incorrectly")
 	public void theUserCompletesRegisterFormIncorrectly() {
+		driver.findElement(By.id("email")).click();
+
+		WebElement email = driver.findElement(By.id("email"));
+		email.clear();
+		email.sendKeys("wrongemail");
 		
+		WebElement form = driver.findElement(By.id("submit"));
+		form.submit();
+	}
+	
+	@Then("the user fatal register message appears")
+	public void theUserFatalRegisterMessageAppears() {
+		String msg = driver.findElement(By.xpath("//p[@font-family='default' and @color='#3a3a3a' "
+				+ "and @font-size='xs' and @data-test='email__error']")).getText();
+		assertTrue(msg.contains("Por favor, añade un correo electrónico válido."));		
 	}
 	
 	@When("the user completes register form correctly")
 	public void theUserCompletesRegisterFromCorrectly() {
+		driver.findElement(By.id("email")).click();
+
+		WebElement email = driver.findElement(By.id("email"));
+		email.clear();
+		email.sendKeys("peportinlopez@gmail.com");
 		
+		WebElement form = driver.findElement(By.id("submit"));
+		form.submit();	
 	}
 	
-	@Then("the user register message appears")
-	public void theUserRegisterMessageAppears() {
-		// Test ToDo
+	@Then("the user verification register message appears")
+	public void theUserVerificationRegisterMessageAppears() {
+		String msg = driver.findElement(By.xpath(
+			    "//p[contains(@class, 'sc-8b815c14-0 jqNLmg') and @font-family='default' and @color='#3a3a3a' and @font-size='sm']"
+			)).getText();
+		assertTrue(msg.contains("Comprueba tu buzón de correo. Introduce el código de verificación de 6 dígitos enviado a"));
 	}
 }
