29b233d677f46bc95ed4e96098a22b552ef9563a
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2024-12-17 12:13:36 +0100

    Update SearchProduct

--- src/test/java/steps/SearchProductSteps.java
+++ src/test/java/steps/SearchProductSteps.java
@@ -20,7 +20,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.time.Duration; // Importa la clase Duration
 
-
 public class SearchProductSteps {
 	
 	WebDriver driver = HomePageSteps.getDriver();
