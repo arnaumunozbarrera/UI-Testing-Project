51c7cc077475a9a2b02f77b43bac41b78002e9b2
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2024-12-17 17:31:36 +0100

    Update test FilterProduct

--- src/test/java/steps/FilterProductSteps.java
+++ src/test/java/steps/FilterProductSteps.java
@@ -23,13 +23,36 @@ import io.cucumber.java.en.When;
 import java.time.Duration;
 
 public class FilterProductSteps {
+	// Classe que testeja el filtres que apareixen a la pàgina web a la part lateral esquerra pels productes cercats
 	
 	WebDriver driver = HomePageSteps.getDriver();
 	
 	@Then("the product result list1 appears")
 	public void productResultList1Appears() {
-		// Test ToDo
-		assertTrue(true);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(12)); 
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div")));
+		
+		WebElement list = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div"));
+		assertTrue(list != null);
+		
+		WebElement title1 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/section/div[1]/h1/div"));
+		assertTrue(title1 != null);
+		
+		String title1Msg = title1.getText();
+		assertTrue(title1Msg.contains("Resultados de la búsqueda para iphone 11"));
+		
+		WebElement filters = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/aside"));
+		assertTrue(filters != null);
+		
+		WebElement title2 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/aside/div/section/ul/li[1]/div[1]/button/span[1]/span"));
+		String title2Msg = title2.getText();
+		assertEquals(title2Msg, "Categoría");
+		
+		WebElement btn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/section/div[2]/div/div[2]/div[1]/div/div/select/option[1]"));
+		assertTrue(btn != null);
+		
+		String btnMsg = btn.getText();
+		assertEquals(btnMsg, "Mejores resultados");
 	}
 	
 	@When("the user clicks on filter1 button")
@@ -76,7 +99,49 @@ public class FilterProductSteps {
 	
 	@Then("the new product result list appears")
 	public void theNewProductResultListAppears(){
-		// Test ToDo
-		assertTrue(true);
+		WebElement title1 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/section/div[1]/h1/div"));
+		assertTrue(title1 != null);
+		
+		String title1Msg = title1.getText();
+		assertTrue(title1Msg.contains("1 Resultado de la búsqueda para iphone 11"));
+		
+		WebElement filter1 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/aside/div/section/ul/li[2]/div[2]/div/div/div/div/div[1]/div/label/p"));
+		assertTrue(filter1 != null);
+		
+		assertEquals(filter1.getText(), "Reacondicionado (1)");
+		
+		WebElement filter2 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/aside/div/section/ul/li[3]/div[2]/div/div/div/div/div[1]/div/label/p"));
+		assertTrue(filter2 != null);
+		
+		assertEquals(filter2.getText(),"A prueba de salpicaduras (1)");
+		
+		WebElement filter3 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/aside/div/section/ul/li[4]/div[2]/div/div/div/div/div/div/label/p"));
+		assertTrue(filter3 != null);
+		
+		assertEquals(filter3.getText(), "MediaMarkt (1)");
+		
+		WebElement btn1 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/div[1]/aside/button[2]"));
+		assertTrue(btn1 != null);
+		
+		String btn1Msg = btn1.getText();
+		assertEquals(btn1Msg, "Campañas destacadas: Reacondicionado");
+		
+		WebElement btn2 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/div[1]/aside/button[3]"));
+		assertTrue(btn2 != null);
+		
+		String btn2Msg = btn2.getText();
+		assertEquals(btn2Msg, "Especificaciones: A prueba de salpicaduras");
+		
+		WebElement btn3 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/div[1]/aside/button[4]"));
+		assertTrue(btn3 != null);
+		
+		String btn3Msg = btn3.getText();
+		assertEquals(btn3Msg, "Vendedor: MediaMarkt");	
+		
+		WebElement btn4 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/main/div[1]/div/div/div/div[1]/aside/button[1]"));
+		assertTrue(btn4 != null);
+		
+		String btn4Msg = btn4.getText();
+		assertEquals(btn4Msg, "Borrar todo");	
 	}
 }
